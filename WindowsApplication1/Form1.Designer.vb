4dd075e013b43dbf05e4b9cdc6c8b409180880dc
Pradeep Kumar Ravilla <pravilla@hulk.soic.indiana.edu>
2016-05-08 19:53:24 -0400

    VB Complete Task Final

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(486, 262)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(63, 28)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.TextBox8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.TextBox7)
+        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(265, 47)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(734, 474)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bill To Address"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(335, 372)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 48)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(242, 330)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(127, 26)
+        Me.TextBox9.TabIndex = 18
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(96, 330)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(65, 20)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Contact"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(242, 297)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(127, 26)
+        Me.TextBox8.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(94, 297)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 20)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Country"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(242, 261)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(172, 26)
+        Me.TextBox7.TabIndex = 14
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(242, 230)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox6.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(432, 261)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(48, 20)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "State"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(242, 198)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox5.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(94, 258)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "City"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(242, 168)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox4.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(94, 238)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "ZIP Code"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(242, 137)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(94, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Address"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(242, 93)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(94, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Bill To Name"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(242, 46)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(94, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Customer Number"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1148, 570)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Customer Details"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,151 @@
+﻿
+Public Class Form1
+
+    Public Class Customer
+        Private name As String        'customer name'
+        Private id As Integer         'customer id needs to be unique'
+        Private city As String        'current city'
+        Private state As String       'current state'
+        Private zip As String         'current zip'
+        Private contact As String       'phone'
+        Private country As String     'country'
+        Private address As String
+
+        Public Property customerNum() As Integer
+            Get
+                Return id
+            End Get
+            Set(custID As Integer)
+                id = custID
+            End Set
+        End Property
+
+        Public Property customerName() As String
+            Get
+                Return Name
+            End Get
+            Set(custName As String)
+                name = custName
+            End Set
+        End Property
+
+
+        Public Property customerCity() As String
+            Get
+                Return city
+            End Get
+            Set(custCity As String)
+                city = custCity
+            End Set
+        End Property
+
+        Public Property customerState() As String
+            Get
+                Return state
+            End Get
+            Set(custState As String)
+                state = custState
+            End Set
+        End Property
+
+        Public Property customerZip() As String
+            Get
+                Return zip
+            End Get
+            Set(custZip As String)
+                zip = custZip
+            End Set
+        End Property
+
+        Public Property customerCountry() As String
+            Get
+                Return country
+            End Get
+            Set(custCountry As String)
+                country = custCountry
+            End Set
+        End Property
+
+        ' returns address object
+        Public Property customerAddress() As String
+            Get
+                Return address
+            End Get
+            Set(custAddress As String)
+                address = custAddress
+            End Set
+        End Property
+        ' returns customer contact
+        Public Property customerContact() As String
+            Get
+                Return contact
+            End Get
+            Set(custContact As String)
+                contact = custContact
+            End Set
+        End Property
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal customerName As String, ByVal customerId As Integer, ByVal customerCity As String, ByVal customerState As String, ByVal customerZip As String, ByVal customerContact As String, ByVal customerCountry As String, ByVal line1 As String, Optional ByVal line2 As String = "", Optional ByVal line3 As String = "")
+            name = customerName
+            id = customerId
+            city = customerCity
+            state = customerState
+            zip = customerZip
+            contact = customerContact
+            country = customerCountry
+            address = line1 + "\n" + line2 + "\n" + line3
+        End Sub
+
+    End Class
+
+
+    'Public CustObj As Customer = New Customer()
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim BillingIdSource As New AutoCompleteStringCollection()
+        BillingIdSource.AddRange(New String() _
+                                  { _
+                                    "12345", _
+                                    "12346", _
+                                    "12347" _
+                                  })
+        Dim BillingContactSource As New AutoCompleteStringCollection()
+        BillingContactSource.AddRange(New String() _
+                                  { _
+                                    "1234589334", _
+                                    "9849217134", _
+                                    "2435678756" _
+                                  })
+
+        TextBox1.AutoCompleteCustomSource = BillingIdSource
+        TextBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend
+        TextBox1.AutoCompleteSource = AutoCompleteSource.CustomSource
+
+        TextBox9.AutoCompleteCustomSource = BillingContactSource
+        TextBox9.AutoCompleteMode = AutoCompleteMode.SuggestAppend
+        TextBox9.AutoCompleteSource = AutoCompleteSource.CustomSource
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim CustObj As Customer = New Customer()
+        CustObj.customerName = TextBox2.Text
+        CustObj.customerNum = TextBox1.Text
+        CustObj.customerCity = TextBox7.Text
+        CustObj.customerZip = TextBox6.Text
+        CustObj.customerState = ComboBox1.Text
+        CustObj.customerAddress = TextBox4.Text + "\n" + TextBox5.Text + "\n" + TextBox6.Text
+        CustObj.customerContact = TextBox9.Text
+
+
+    End Sub
+
+    Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs) Handles TextBox9.TextChanged
+
+    End Sub
+End Class
